ba36e90d3e16c031e6989965e4c7d7ee8550500c
Brian Takita <dev@truecar.com>
2011-05-09 20:21:08 -0700

    Allow unquoted command to be passed in as a command line arg.

--- lib/zk-ephemeral-wrapper.rb
+++ lib/zk-ephemeral-wrapper.rb
@@ -1,7 +1,7 @@
 require "zookeeper"
 
 class ZkEphemeralWrapper
-  def self.call(zookeeper_hosts, full_node, cmd)
+  def self.call(zookeeper_hosts, full_node, *cmd)
     z = Zookeeper.new(zookeeper_hosts)
     split_nodes = full_node.split("/")[1..-1]
     parent_node = split_nodes[0..-2].inject("") do |memo, dir_node|
@@ -10,7 +10,7 @@ class ZkEphemeralWrapper
       end
     end
     z.create(:path => File.join(parent_node, split_nodes[-1]), :ephemeral => true)
-    system(cmd)
+    system(cmd.join(" "))
     exit
   end
 end
--- spec/zk-ephemeral-wrapper_spec.rb
+++ spec/zk-ephemeral-wrapper_spec.rb
@@ -2,29 +2,42 @@ require File.expand_path("#{File.dirname(__FILE__)}/spec_helper")
 
 describe ZkEphemeralWrapper do
   describe ".call" do
-    context "when passed a top-level node" do
-      it "creates an ephemeral node for the top-level node" do
-        mock.strong(Zookeeper).new("127.0.0.1:2181") do
-          mock!.create(:path => "/foobar", :ephemeral => true)
+    describe "node" do
+      context "when passed a top-level node" do
+        it "creates an ephemeral node for the top-level node" do
+          mock.strong(Zookeeper).new("127.0.0.1:2181") do
+            mock!.create(:path => "/foobar", :ephemeral => true)
+          end
+          mock.strong(ZkEphemeralWrapper).system("my-program")
+          mock.strong(ZkEphemeralWrapper).exit
+          ZkEphemeralWrapper.call("127.0.0.1:2181", "/foobar", "my-program")
         end
-        mock.strong(ZkEphemeralWrapper).system("my-program")
-        mock.strong(ZkEphemeralWrapper).exit
-        ZkEphemeralWrapper.call("127.0.0.1:2181", "/foobar", "my-program")
+      end
+
+      context "when passed a node in a directory" do
+        it "creates nodes for each directory and an ephemeral node leaf node" do
+          mock.strong(Zookeeper).new("127.0.0.1:2181") do
+            mock! do |z|
+              z.create(:path => "/foo")
+              z.create(:path => "/foo/bar")
+              z.create(:path => "/foo/bar/baz", :ephemeral => true)
+            end
+          end
+          mock.strong(ZkEphemeralWrapper).system("my-program")
+          mock.strong(ZkEphemeralWrapper).exit
+          ZkEphemeralWrapper.call("127.0.0.1:2181", "/foo/bar/baz", "my-program")
+        end
       end
     end
 
-    context "when passed a node in a directory" do
-      it "creates nodes for each directory and an ephemeral node leaf node" do
+    describe "command" do
+      it "accepts concats multiple cmd parts" do
         mock.strong(Zookeeper).new("127.0.0.1:2181") do
-          mock! do |z|
-            z.create(:path => "/foo")
-            z.create(:path => "/foo/bar")
-            z.create(:path => "/foo/bar/baz", :ephemeral => true)
-          end
+          mock!.create(:path => "/foobar", :ephemeral => true)
         end
-        mock.strong(ZkEphemeralWrapper).system("my-program")
+        mock.strong(ZkEphemeralWrapper).system("my program runs")
         mock.strong(ZkEphemeralWrapper).exit
-        ZkEphemeralWrapper.call("127.0.0.1:2181", "/foo/bar/baz", "my-program")
+        ZkEphemeralWrapper.call("127.0.0.1:2181", "/foobar", "my", "program", "runs")        
       end
     end
   end
