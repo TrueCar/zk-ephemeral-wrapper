e1b6bd55e39d83d809b9334f6f1eddc2fb96abee
Brian Takita <dev@truecar.com>
2011-05-11 11:12:18 -0700

    Allow multiple nodes to be defined (comma delimited).

--- lib/zk-ephemeral-wrapper.rb
+++ lib/zk-ephemeral-wrapper.rb
@@ -1,15 +1,17 @@
 require "zookeeper"
 
 class ZkEphemeralWrapper
-  def self.call(zookeeper_hosts, full_node, *cmd)
+  def self.call(zookeeper_hosts, full_nodes, *cmd)
     z = Zookeeper.new(zookeeper_hosts)
-    split_nodes = full_node.split("/")[1..-1]
-    parent_node = split_nodes[0..-2].inject("") do |memo, dir_node|
-      File.join(memo, dir_node).tap do |dir|
-        z.create(:path => dir)
+    full_nodes.split(",").each do |full_node|
+      split_nodes = full_node.split("/")[1..-1]
+      parent_node = split_nodes[0..-2].inject("") do |memo, dir_node|
+        File.join(memo, dir_node).tap do |dir|
+          z.create(:path => dir)
+        end
       end
+      z.create(:path => File.join(parent_node, split_nodes[-1]), :ephemeral => true)
     end
-    z.create(:path => File.join(parent_node, split_nodes[-1]), :ephemeral => true)
     exit system(cmd.join(" "))
   end
 end
@@ -20,7 +22,7 @@ if __FILE__ == $0
     Wraps a program with a zookeeper ephemeral node.
 
     Usage:
-      zk-ephemeral-wrapper [options] <comma_delimited_zookeeper_hosts> <node_path> <*cmd>
+      zk-ephemeral-wrapper [options] <comma_delimited_zookeeper_hosts> <comma_delimited_node_paths> <*cmd>
     TEXT
     exit 1
   end
--- spec/zk-ephemeral-wrapper_spec.rb
+++ spec/zk-ephemeral-wrapper_spec.rb
@@ -8,8 +8,8 @@ describe ZkEphemeralWrapper do
           mock.strong(Zookeeper).new("127.0.0.1:2181") do
             mock!.create(:path => "/foobar", :ephemeral => true)
           end
-          mock.strong(ZkEphemeralWrapper).system("my-program")
-          mock.strong(ZkEphemeralWrapper).exit
+          mock.strong(ZkEphemeralWrapper).system("my-program") {0}
+          mock.strong(ZkEphemeralWrapper).exit(0)
           ZkEphemeralWrapper.call("127.0.0.1:2181", "/foobar", "my-program")
         end
       end
@@ -23,9 +23,29 @@ describe ZkEphemeralWrapper do
               z.create(:path => "/foo/bar/baz", :ephemeral => true)
             end
           end
-          mock.strong(ZkEphemeralWrapper).system("my-program")
-          mock.strong(ZkEphemeralWrapper).exit
+          mock.strong(ZkEphemeralWrapper).system("my-program") {0}
+          mock.strong(ZkEphemeralWrapper).exit(0)
           ZkEphemeralWrapper.call("127.0.0.1:2181", "/foo/bar/baz", "my-program")
+        end
+      end
+    end
+
+    describe "node paths" do
+      context "when passed a comma-delimited list of node paths" do
+        it "creates nodes for path" do
+          mock.strong(Zookeeper).new("127.0.0.1:2181") do
+            mock! do |z|
+              z.create(:path => "/foo")
+              z.create(:path => "/foo/bar")
+              z.create(:path => "/foo/bar/baz", :ephemeral => true)
+              z.create(:path => "/one")
+              z.create(:path => "/one/two")
+              z.create(:path => "/one/two/three", :ephemeral => true)
+            end
+          end
+          mock.strong(ZkEphemeralWrapper).system("my-program") {0}
+          mock.strong(ZkEphemeralWrapper).exit(0)
+          ZkEphemeralWrapper.call("127.0.0.1:2181", "/foo/bar/baz,/one/two/three", "my-program")
         end
       end
     end
@@ -35,8 +55,8 @@ describe ZkEphemeralWrapper do
         mock.strong(Zookeeper).new("127.0.0.1:2181") do
           mock!.create(:path => "/foobar", :ephemeral => true)
         end
-        mock.strong(ZkEphemeralWrapper).system("my program runs")
-        mock.strong(ZkEphemeralWrapper).exit
+        mock.strong(ZkEphemeralWrapper).system("my program runs") {0}
+        mock.strong(ZkEphemeralWrapper).exit(0)
         ZkEphemeralWrapper.call("127.0.0.1:2181", "/foobar", "my", "program", "runs")        
       end
     end
